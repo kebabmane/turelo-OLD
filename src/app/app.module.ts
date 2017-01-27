6c75d91c4aa7bca7c62c7dd7b94539119d9073de
Rhys Evans <evans.rhys@gmail.com>
2017-01-27 23:17:11 +1100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import {MomentModule} from 'angular2-moment';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
