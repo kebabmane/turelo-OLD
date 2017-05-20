836443fef85f1795ca7cbca3ce7b0465f24dacd7
Rhys Evans <rjevans@Rhyss-MacBook.local>
2017-05-20 18:19:01 +1000

    fixed a routing bug, func() implemented the authGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,15 @@ import { FeedsComponent } from './feeds/feeds.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { EntryComponent } from './entry/entry.component';
 import { AuthService } from '../auth/services/auth-service';
+import { AuthGuard } from '../auth/guards/auth-guard';
+import { UnauthGuard } from '../auth/guards/unauth-guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: MainComponent, children:
+  { path: '', component: MainComponent, canActivate: [AuthGuard], children:
   [
     { path: '', redirectTo: '/timeline', pathMatch: 'full' },
+    { path: 'timeline', component: TimelineComponent },
     { path: 'feeds', component: FeedsComponent },
     { path: 'feeds/feed_entries/:id', component: FeedEntriesComponent },
     { path: 'feeds/feed_entries/:id/:link', component: EntryComponent }
